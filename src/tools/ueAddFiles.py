d04b28dacefbf2114bce1ab8fa3ae90fe7033322
Thomas Stevenson <dr.dogbiscuit@gmail.com>
2012-02-09 18:09:24 +0000

    Updated ueAddFiles

--- src/tools/ueAddFiles.py
+++ src/tools/ueAddFiles.py
@@ -1,61 +1,51 @@
 #!/usr/bin/python
 
-import sys, os, shutil
+import sys, os
 import glob, getopt
 
-import ueCore.AssetUtils as ueAssetUtils
-import ueCore.CreateUtils as ueCreateUtils
+import ueClient, ueSpec
 
-global config
+import ueCore.AssetUtils as ueAssetUtils
+import ueCore.Create as ueCreate
+import ueCore.FileUtils as ueFileUtils
+
+config = {}
 
 def addFiles():
-#    if not "files" in config:
-#        print "ERROR: No files given"
-#        sys.exit(2)
-
-#    if not "element" in config:
-#        print "ERROR: No element given"
-#        sys.exit(2)
-
-    e = sys.argv[1].split(":")
-
-    if not len(e) == 6:
-        print "ERROR: Incomplete element"
+    if not "files" in config:
+        print "ERROR: No files given"
         sys.exit(2)
 
-    proj = e[0]
-    grp = e[1]
-    asst = e[2]
-    elclass = e[5]
-    eltype = e[4]
-    name = e[3]
+    if not "spec" in config:
+        print "ERROR: No element spec given"
+        sys.exit(2)
 
-    files = sys.argv[2:]
+    d = ueAssetUtils.getElement(config["spec"])
+    if d == {}:
+        d = ueCreate.createElement(config["spec"])
 
-    d = ueAssetUtils.getElement(proj, grp, asst, elclass, eltype, name)
-    if d == None:
-        d = ueCreateUtils.createElement(proj, grp, asst, elclass, eltype, name)
+    p = ueCreate.createVersion(config["spec"])
 
-    vers = ueAssetUtils.getVersions(proj, grp, asst, elclass, eltype, name)
-    fName = ueCreateUtils.getElementName(proj, grp, asst, elclass, eltype, name, len(vers)+1)
+    config["spec"].vers = p["version"]
+
+    files = glob.glob(config["files"])
+
+    path = p["path"]
+    name = ueAssetUtils.getElementName(config["spec"])
     ext = files[0].split(".")[-1]
 
-    p = ueCreateUtils.createVersion(proj, grp, asst, elclass, eltype, name, len(vers)+1,
-                                    source=files[0])
-
     if len(files) == 1:
-        fPath = os.path.join(p["path"], "%s.%s" % (fName, ext))
-        shutil.copy(files[0], fPath)
+        dest = os.path.join(path, "%s.%s" % (name, ext))
+        ueFileUtils.copyFile(files[0], dest)
     else:
-        for f in files:
-            fPath = os.path.join(p["path"], "%s.%04d.%s" % (fName, files.index(f)+1, ext))
-            shutil.copy(f, fPath)
+        for f in sorted(files):
+            dest = os.path.join(path, "%s.%04d.%s" % (name, files.index(f)+1, ext))
+            ueFileUtils.copyFile(f, dest)
+
 
 def parse():
-    global config
-
-    sArgs = "h"
-    lArgs = ["help"]
+    sArgs = "hs:f:"
+    lArgs = ["help", "spec=", "files="]
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], sArgs, lArgs)
@@ -63,24 +53,26 @@ def parse():
         print "ERROR: Parsing argument (%s)" % e
         sys.exit(2)
 
-    config = {}
-
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
             sys.exit(0)
+        elif o in ("-s", "--spec"):
+            config["spec"] = ueSpec.Spec(a)
+        elif o in ("-f", "--files"):
+            config["files"] = a
         else:
             print "ERROR: Unrecognised argument '%s'" % o
             sys.exit(2)
 
 
 def usage():
-    print "Usage: %s [ELEMENT NAME] [FILES]" % os.path.basename(sys.argv[0])
+    print "Usage: %s -s [ELEMENT] -f [FILES]" % os.path.basename(sys.argv[0])
     print "Adds files to a ue element."
     print ""
-    print "\t[ELEMENT NAME]      ue element name, e.g."
+    print "\t-s, --spec          ue element name, e.g."
     print "\t                    proj:grp:asst:name:type:class"
-    print "\t[FILES]             Path to file(s). Can include"
+    print "\t-f, --files         Path to file(s). Can include"
     print "\t                    wildcards."
     print "\t-h, --help          Print this help"
 
@@ -89,6 +81,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         usage()
         sys.exit(0)
+
+    ueClient.Client()
 
     parse()
     addFiles()
