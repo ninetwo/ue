d532a58c02a84bea2118c4eb3b3474977c81dd87
Thomas Stevenson <dr.dogbiscuit@gmail.com>
2012-02-13 13:28:58 +0000

    Added open dialog to Nuke read and write gizmos

--- src/ueNuke/Open.py
+++ src/ueNuke/Open.py
@@ -8,6 +8,8 @@ import ueCore.AssetUtils as ueAssetUtils
 import ueCommon.Open as ueCommonOpen
 
 __ueclasses__ = ["ns"]
+__ueclasses_read__ = ["nr", "mr", "cel", "bg"]
+__ueclasses_write__ = ["nr", "mr"]
 
 def ueOpen():
     p = nukescripts.registerWidgetAsPanel("ueCommonOpen.Open", "ueOpen",
@@ -21,6 +23,43 @@ def ueOpen():
         nkFile = ueAssetUtils.getElementName(spec)
         nuke.scriptOpen(os.path.join(nkPath, nkFile+".nk"))
         nuke.tprint("Opened %s" % spec)
+
+    nukescripts.unregisterPanel("ue.panel.ueOpen", lambda: "return")
+
+def ueOpenRead():
+    p = nukescripts.registerWidgetAsPanel("ueCommonOpen.Open", "ueOpen",
+                                          "ue.panel.ueOpen", create=True)
+    p.setMinimumSize(600, 920)
+    ueCommonOpen.setClasses(__ueclasses_read__)
+
+    if p.showModalDialog():
+        spec = ueCommonOpen.getValues()
+        n = nuke.thisNode()
+        n.knob("proj").setValue(spec.proj)
+        n.knob("grp").setValue(spec.grp)
+        n.knob("asst").setValue(spec.asst)
+        n.knob("elclass").setValue(spec.elclass)
+        n.knob("eltype").setValue(spec.eltype)
+        n.knob("elname").setValue(spec.elname)
+        n.knob("vers").setValue(int(spec.vers))
+
+    nukescripts.unregisterPanel("ue.panel.ueOpen", lambda: "return")
+
+def ueOpenWrite():
+    p = nukescripts.registerWidgetAsPanel("ueCommonOpen.Open", "ueOpen",
+                                          "ue.panel.ueOpen", create=True)
+    p.setMinimumSize(600, 920)
+    ueCommonOpen.setClasses(__ueclasses_write__)
+
+    if p.showModalDialog():
+        spec = ueCommonOpen.getValues()
+        n = nuke.thisNode()
+        n.knob("proj").setValue(spec.proj)
+        n.knob("grp").setValue(spec.grp)
+        n.knob("asst").setValue(spec.asst)
+        n.knob("elclass").setValue(spec.elclass)
+        n.knob("eltype").setValue(spec.eltype)
+        n.knob("elname").setValue(spec.elname)
 
     nukescripts.unregisterPanel("ue.panel.ueOpen", lambda: "return")
 
