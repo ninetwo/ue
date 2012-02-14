9721a4089eed51fe5e5071765686abe0f6c8bebc
Thomas Stevenson <dr.dogbiscuit@gmail.com>
2012-02-14 11:55:29 +0000

    Allow frame ranges in nuke renders

--- src/ueNuke/Render.py
+++ src/ueNuke/Render.py
@@ -10,23 +10,33 @@ import ueNuke.Save as ueNukeSave
 import ueNuke.Utilities as ueNukeUtils
 import ueCommon.Render as ueCommonRender
 
-def ueRender():
+def ueRender(n):
     p = nukescripts.registerWidgetAsPanel("ueCommonRender.Render", "ueRender",
                                           "ue.panel.ueRender", create=True)
 
     if p.showModalDialog():
-        v = ueCommonRender.getValues()
-        nuke.tprint(v)
+        renderOpts = ueCommonRender.getValues()
+        nuke.tprint(renderOpts)
 
     nukescripts.unregisterPanel("ue.panel.ueRender", lambda: "return")
 
-def preRender():
+    if renderOpts[0] == 0:
+        preRender(n)
+        if n.knob("limit_range").value():
+            first = n.knob("first").value()
+            last = n.knob("last").value()
+        else:
+            first = nuke.root().knob("first_frame").value()
+            last = nuke.root().knob("last_frame").value()
+        nuke.execute(n.name()+"."+n.knob("format").value(),
+                     int(first), int(last), 1)
+        postRender(n)
+
+def preRender(n):
     root = nuke.root()
 
     if root.name() == "Root":
         ueNukeSave.ueSaveAs()
-
-    n = nuke.thisParent()
 
     sourceSpec = ueSpec.Spec(root.knob("ueproj").value(),
                              root.knob("uegrp").value(),
@@ -57,7 +67,7 @@ def preRender():
     rName = ueAssetUtils.getElementName(destSpec)
     rPath = os.path.join(p["path"], rName+".%04d.exr")
 
-    nuke.thisNode().knob("file").setValue(rPath)
+    nuke.toNode(n.name()+"."+n.knob("format").value()).knob("file").setValue(rPath)
 
     dbMeta = {}
     dbMeta["comment"] = "Auto-save of render %s" % str(destSpec)
@@ -67,9 +77,7 @@ def preRender():
 
     nuke.tprint("Rendering %s to %s ..." % (str(destSpec), os.path.dirname(rPath)))
 
-def postRender():
-    n = nuke.thisParent()
-
+def postRender(n):
     destSpec = ueSpec.Spec(n.knob("proj").value(),
                            n.knob("grp").value(),
                            n.knob("asst").value(),
