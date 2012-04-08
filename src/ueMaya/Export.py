096a58a2c3dc55aa06a70c6ae819b622e731ecf6
Thomas Stevenson <dr.dogbiscuit@gmail.com>
2012-04-08 12:17:47 +0100

    adding further options to maya export

--- src/ueMaya/Export.py
+++ src/ueMaya/Export.py
@@ -35,6 +35,10 @@ class Export(QtGui.QMainWindow):
         self.exportMenu = QtGui.QListWidget()
         self.itemMenu = QtGui.QListWidget()
         self.itemMenu.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+        self.exportAsSeparateElements = QtGui.QCheckBox("Export selection as seperate elements?")
+        self.exportAsSeparateElements.setChecked(False)
+        self.exportCache = QtGui.QCheckBox("Export cache?")
+        self.exportCache.setChecked(True)
 
         for et in __exportTypes__:
             self.exportMenu.addItem(QtGui.QListWidgetItem(et))
@@ -42,11 +46,17 @@ class Export(QtGui.QMainWindow):
         exportWidget = QtGui.QGroupBox("Export")
         exportWidget.setLayout(QtGui.QGridLayout())
 
+        exportOptionsWidget = QtGui.QGroupBox("Export Options")
+        exportOptionsWidget.setLayout(QtGui.QVBoxLayout())
+
         exportWidget.layout().addWidget(QtGui.QLabel("Export"), 0, 0)
         exportWidget.layout().addWidget(self.exportMenu, 1, 0)
         exportWidget.layout().addWidget(QtGui.QLabel("Item"), 0, 1)
         exportWidget.layout().addWidget(self.itemMenu, 1, 1)
-        
+
+        exportOptionsWidget.layout().addWidget(self.exportAsSeparateElements)
+        exportOptionsWidget.layout().addWidget(self.exportCache)
+
         self.saveWidget = ueCommonSave.Save()
         buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok|
                                            QtGui.QDialogButtonBox.Cancel)
@@ -54,6 +64,7 @@ class Export(QtGui.QMainWindow):
         centralWidget = QtGui.QWidget()
         centralWidget.setLayout(QtGui.QVBoxLayout())
         centralWidget.layout().addWidget(exportWidget)
+        centralWidget.layout().addWidget(exportOptionsWidget)
         centralWidget.layout().addWidget(self.saveWidget)
         centralWidget.layout().addWidget(buttonBox)
 
@@ -88,28 +99,65 @@ class Export(QtGui.QMainWindow):
 
     def export(self):
         spec, dbMeta = ueCommonSave.getValues()
+
+        # Get the selected nodes from the dialog list
         selection = []
         for i in self.itemMenu.selectedItems():
-            selection.append(i.text())
-        maya.cmds.select(selection, ne=True, r=True)
+            selection.append(str(i.text()))
+
+        # If we're exporting all our seleted items into one element,
+        # add the entire list into another list so when we iterate over
+        # it, we return the list once and export everything
+        if not self.exportAsSeparateElements.isChecked():
+            newSelection = []
+            newSelection.append(selection)
+            selection = newSelection
+
+        # Iterate over our selection and export
         if spec.elclass == "ms":
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+            for item in selection:
+                if self.exportAsSeparateElements.isChecked():
+                    spec.elname = item
+                maya.cmds.select(item, r=True)
+                ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
         elif spec.elclass == "cam":
-            spec.elclass = "ms"
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
-            spec.elclass = "cam"
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="fbx", export=True)
+            for item in selection:
+                if self.exportAsSeparateElements.isChecked():
+                    spec.elname = item
+                maya.cmds.select(item, r=True)
+                spec.elclass = "ms"
+                ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+                if self.exportCache.isChecked():
+                    spec.elclass = "cam"
+                    ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="fbx", export=True)
         elif spec.elclass == "lgt":
-            spec.elclass = "ms"
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
-            spec.elclass = "lgt"
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="fbx", export=True)
+            for item in selection:
+                if self.exportAsSeparateElements.isChecked():
+                    spec.elname = item
+                maya.cmds.select(item, r=True)
+                spec.elclass = "ms"
+                ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+                if self.exportCache.isChecked():
+                    spec.elclass = "lgt"
+                    ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="fbx", export=True)
         elif spec.elclass == "geo":
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="obj", export=True)
-            spec.elclass = "ms"
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+            for item in selection:
+                if self.exportAsSeparateElements.isChecked():
+                    spec.elname = item
+                maya.cmds.select(item, r=True)
+                spec.elclass = "ms"
+                ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+                if self.exportCache.isChecked():
+                    spec.elclass = "geo"
+                    ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="obj", export=True)
         elif spec.elclass == "mrs":
-            ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+            for item in selection:
+                if self.exportAsSeparateElements.isChecked():
+                    spec.elname = item
+                maya.cmds.select(item, r=True)
+                ueMayaUtils.saveUtility(spec, dbMeta=dbMeta, fileType="ma", export=True)
+
 #        ueFileUtils.deleteFiles(os.path.join(os.path.join(os.getenv("ASST_ROOT"), "tmp", "ueSaveThumbs_*.png")))
+
         self.close()
 
