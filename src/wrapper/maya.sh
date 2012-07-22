27af9d39bb5c9388179bc972c71520cebe60e54e
Thomas Stevenson <dr.dogbiscuit@gmail.com>
2012-07-22 15:29:29 +0100

    Updates to wrappers

--- src/wrapper/maya.sh
+++ src/wrapper/maya.sh
@@ -6,6 +6,7 @@ if [[ $PROJ == "" || $GRP == "" || $ASST == "" ]]; then
 fi
 
 export MAYA_SCRIPT_PATH=$MAYA_SCRIPT_PATH:$PROJ_ROOT/etc/maya
+export MAYA_PLUG_IN_PATH=$MAYA_PLUG_IN_PATH:$UE_PATH/src/ueMaya/nodes
 #export MAYA_SHELF_PATH=$PROJ_ROOT/etc/maya
 export PYTHONPATH=$PYTHONPATH:$PROJ_ROOT/etc/maya:$UE_PATH/tools/maya
 
--- src/wrapper/rv.sh
+++ src/wrapper/rv.sh
@@ -1,6 +1,11 @@
 rv_path=/opt/rv-Linux-x86-64-3.12.14/bin/rv.bin
 
-export LD_LIBRARY_PATH=/opt/rv-Linux-x86-64-3.12.14/libWorking
+if [[ $PROJ == "" || $GRP == "" || $ASST == "" ]]; then
+  echo "Error: uesp before running rv"
+  exit 0
+fi
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rv-Linux-x86-64-3.12.14/libWorking
 
 $rv_path $*
 
